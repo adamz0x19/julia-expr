395a7a13e940a558e9ca893cad9618267c3602b3
Adam <155471138+adamz0x19@users.noreply.github.com>
2024-01-04 20:42:17 +0100

    fractal 2

---
+++ fractal2.jl
@@ -0,0 +1,16 @@
+import Pkg; Pkg.add(url="https://github.com/jofrevalles/JuliaFractals.jl.git"); Pkg.add("CairoMakie")
+
+using JuliaFractals
+using CairoMakie
+CairoMakie.activate!(type = "png")
+
+w = h = 1000
+style = :smooth
+max_it = 400
+c = -0.8 + 0.156im
+
+julia_fractal = julia(c, max_it)
+img = generate_image(julia_fractal, style, w, h, (-2.0, 2.0), (-2.0, 2.0), max_it)
+
+cmap = :algae
+f = visualize(img, :makie; cmap=cmap)
